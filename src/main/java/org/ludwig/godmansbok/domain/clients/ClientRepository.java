ee61db11cca78b208feb988629aa9ebc9d46cb6e
Ludwig <00ludwighahn@gmail.com>
2025-05-31 19:18:04 +0200

    ClientService: Add method to fetch all clients by godman ID

--- src/main/java/org/ludwig/godmansbok/domain/clients/ClientRepository.java
+++ src/main/java/org/ludwig/godmansbok/domain/clients/ClientRepository.java
@@ -1,6 +1,11 @@
 package org.ludwig.godmansbok.domain.clients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    List<Client> findAllByGodmanId(Long godmanId);
 }
