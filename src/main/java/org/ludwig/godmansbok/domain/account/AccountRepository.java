1f637e6caf3d1e17c0ba0bbacadd37115a192bf5
Ludwig <00ludwighahn@gmail.com>
2025-05-31 20:53:06 +0200

    AccountRepository: Add method to find all accounts by client ID

--- src/main/java/org/ludwig/godmansbok/domain/account/AccountRepository.java
+++ src/main/java/org/ludwig/godmansbok/domain/account/AccountRepository.java
@@ -1,5 +1,9 @@
 package org.ludwig.godmansbok.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long>{
+    List<Account> findAllByClientId(Long clientId);
 }
