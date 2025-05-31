24310ca251f3882b7030f4c4739c3e2e8068c4f1
Ludwig <00ludwighahn@gmail.com>
2025-05-31 22:46:05 +0200

    OtherAssetRepository: Add method to find all other assets by client ID

--- src/main/java/org/ludwig/godmansbok/domain/otherasset/OtherAssetRepository.java
+++ src/main/java/org/ludwig/godmansbok/domain/otherasset/OtherAssetRepository.java
@@ -2,5 +2,8 @@ package org.ludwig.godmansbok.domain.otherasset;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OtherAssetRepository extends JpaRepository<OtherAsset, Long> {
+    List<OtherAsset> findAllByClientId(Long clientId);
 }
