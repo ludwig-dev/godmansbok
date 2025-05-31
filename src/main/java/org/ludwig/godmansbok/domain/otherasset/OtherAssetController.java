c981cdd01fbde9fce7a79d0c110a287ae84e6387
Ludwig <00ludwighahn@gmail.com>
2025-05-31 22:38:48 +0200

    OtherAssetController: Add endpoint to create other asset

---
+++ src/main/java/org/ludwig/godmansbok/domain/otherasset/OtherAssetController.java
@@ -0,0 +1,30 @@
+package org.ludwig.godmansbok.domain.otherasset;
+
+import jakarta.validation.Valid;
+import org.ludwig.godmansbok.domain.otherasset.dto.OtherAssetDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clients/{clientId}/other-assets")
+public class OtherAssetController {
+
+    private final OtherAssetService otherAssetService;
+
+    public OtherAssetController(OtherAssetService otherAssetService) {
+        this.otherAssetService = otherAssetService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OtherAssetDTO createOtherAsset(
+            @PathVariable Long clientId,
+            @Valid @RequestBody OtherAssetDTO dto,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        Long godmanId = Long.parseLong(userDetails.getUsername());
+        return OtherAssetDTO.toDto(otherAssetService.createOtherAsset(godmanId, clientId, dto));
+    }
+}
