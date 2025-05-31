b3db7589093bdd9eca8173e2c19ce9fb1c758ff5
Ludwig <00ludwighahn@gmail.com>
2025-05-31 17:53:46 +0200

    GodmanRepository: Remove unused `findByUsernameContainingIgnoreCase` method

--- src/main/java/org/ludwig/godmansbok/godman/GodmanRepository.java
+++ src/main/java/org/ludwig/godmansbok/godman/GodmanRepository.java
@@ -3,12 +3,10 @@ package org.ludwig.godmansbok.godman;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GodmanRepository extends JpaRepository<Godman, Long> {
     Optional<Godman> findByUsernameIgnoreCase(String username);
     Optional<Godman> findByEmailIgnoreCase(String email);
-    List<Godman> findByUsernameContainingIgnoreCase(String username);
 }
