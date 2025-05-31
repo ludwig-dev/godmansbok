e399d35013b9ca18f3e1c3d95ac1a34c21d7d7c8
Ludwig <00ludwighahn@gmail.com>
2025-05-31 21:43:44 +0200

    TransactionDTO: Add method to convert list of transactions to DTOs

--- src/main/java/org/ludwig/godmansbok/domain/transactions/dto/TransactionDTO.java
+++ src/main/java/org/ludwig/godmansbok/domain/transactions/dto/TransactionDTO.java
@@ -4,9 +4,14 @@ import org.ludwig.godmansbok.domain.transactions.Transaction;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 public record TransactionDTO(Long id, LocalDate date, BigDecimal amount, String type, String description, String attachmentNumber) {
     public static TransactionDTO toDto(Transaction transaction) {
         return new TransactionDTO(transaction.getId(), transaction.getDate(), transaction.getAmount(), transaction.getType(), transaction.getDescription(), transaction.getAttachmentNumber());
+    }
+
+    public static List<TransactionDTO> toDtos(List<Transaction> transactions) {
+        return transactions.stream().map(TransactionDTO::toDto).toList();
     }
 }
