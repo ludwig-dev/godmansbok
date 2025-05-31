fe41ae6b2e9ebdc03c9e1f3d1ac7db4dcb0bdeb0
Ludwig <00ludwighahn@gmail.com>
2025-05-31 21:36:00 +0200

    TransactionController: Add endpoint to create a new transaction

---
+++ src/main/java/org/ludwig/godmansbok/domain/transactions/TransactionController.java
@@ -0,0 +1,30 @@
+package org.ludwig.godmansbok.domain.transactions;
+
+import jakarta.validation.Valid;
+import org.ludwig.godmansbok.domain.transactions.dto.TransactionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clients/{clientId}/accounts/{accountId}/transactions")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransactionDTO createTransaction(
+            @PathVariable Long clientId,
+            @PathVariable Long accountId,
+            @Valid @RequestBody TransactionDTO dto,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        Long godmanId = Long.parseLong(userDetails.getUsername());
+        return TransactionDTO.toDto(transactionService.createTransaction(godmanId, clientId, accountId, dto));
+    }
+}
