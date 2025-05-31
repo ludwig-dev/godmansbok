53251e854d10824456917a496cae49b3dd0eaec1
Ludwig <00ludwighahn@gmail.com>
2025-05-31 17:50:49 +0200

    GodmanRepository: annotated `@Repository`

--- src/main/java/org/ludwig/godmansbok/godman/GodmanRepository.java
+++ src/main/java/org/ludwig/godmansbok/godman/GodmanRepository.java
@@ -1,10 +1,12 @@
 package org.ludwig.godmansbok.godman;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface GodmanRepository extends JpaRepository<Godman, Long> {
     Optional<Godman> findByUsernameIgnoreCase(String username);
     Optional<Godman> findByEmailIgnoreCase(String email);
