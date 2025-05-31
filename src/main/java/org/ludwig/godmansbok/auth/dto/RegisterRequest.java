52d4fae07eb2460c8ce0879e06ac713c7c178360
Ludwig <00ludwighahn@gmail.com>
2025-05-31 17:00:00 +0200

    Add RegisterRequest DTO with validation and conversion to Godman

---
+++ src/main/java/org/ludwig/godmansbok/auth/dto/RegisterRequest.java
@@ -0,0 +1,34 @@
+package org.ludwig.godmansbok.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.ludwig.godmansbok.godman.Godman;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 3)
+    private String password;
+
+    public Godman toGodman() {
+        Godman godman = new Godman();
+        godman.setUsername(username);
+        godman.setEmail(email);
+        godman.setPassword(password);
+        return godman;
+    }
+}
