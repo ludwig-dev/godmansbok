837d45bc4a036747f092169013bf28b70f563c62
Ludwig <00ludwighahn@gmail.com>
2025-05-31 19:17:13 +0200

    ClientDTO: Add static method to convert a list of Clients to DTOs

--- src/main/java/org/ludwig/godmansbok/domain/clients/dto/ClientDTO.java
+++ src/main/java/org/ludwig/godmansbok/domain/clients/dto/ClientDTO.java
@@ -2,8 +2,14 @@ package org.ludwig.godmansbok.domain.clients.dto;
 
 import org.ludwig.godmansbok.domain.clients.Client;
 
+import java.util.List;
+
 public record ClientDTO(String name, String personalNumber) {
     public static ClientDTO toDto(Client client) {
         return new ClientDTO(client.getName(), client.getPersonalNumber());
+    }
+
+    public static List<ClientDTO> toDtos(List<Client> clients) {
+        return clients.stream().map(ClientDTO::toDto).toList();
     }
 }
