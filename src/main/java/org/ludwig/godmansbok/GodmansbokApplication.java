e344a0970eb5a9195b4e1037f251ffc85fab2d9c
Ludwig <00ludwighahn@gmail.com>
2025-05-31 15:15:15 +0200

    spring init

---
+++ src/main/java/org/ludwig/godmansbok/GodmansbokApplication.java
@@ -0,0 +1,13 @@
+package org.ludwig.godmansbok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodmansbokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodmansbokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ludwig/godmansbok/GodmansbokApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ludwig.godmansbok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodmansbokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
