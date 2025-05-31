84ad9dfd3a1bb837a529e7e23d69585a12ee8c3d
Ludwig <00ludwighahn@gmail.com>
2025-05-31 19:07:12 +0200

    Create ClientController with endpoint to create a new client

---
+++ src/main/java/org/ludwig/godmansbok/domain/clients/ClientController.java
@@ -0,0 +1,25 @@
+package org.ludwig.godmansbok.domain.clients;
+
+import org.ludwig.godmansbok.domain.clients.dto.ClientDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClientDTO createClient(@RequestBody ClientDTO dto,
+                                  @AuthenticationPrincipal UserDetails userDetails) {
+        Long godmanId = Long.parseLong(userDetails.getUsername());
+        return ClientDTO.toDto(clientService.createClient(godmanId, dto));
+    }
+}
