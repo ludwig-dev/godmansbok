28c0b803acb297eb10dcab08b6265e98b44eecbe
Ludwig <00ludwighahn@gmail.com>
2025-05-31 21:44:08 +0200

    TransactionController: Add endpoint to list all transactions for an account

--- src/main/java/org/ludwig/godmansbok/domain/transactions/TransactionService.java
+++ src/main/java/org/ludwig/godmansbok/domain/transactions/TransactionService.java
@@ -9,6 +9,8 @@ import org.ludwig.godmansbok.exceptions.NotAuthorizedException;
 import org.ludwig.godmansbok.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TransactionService {
 
@@ -48,6 +50,24 @@ public class TransactionService {
         transaction.setAttachmentNumber(dto.attachmentNumber());
         transaction.setAccount(account);
         return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getAllTransactions(Long godmanId,
+                                                Long clientId,
+                                                Long accountId) {
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+        if (!client.getGodman().getId().equals(godmanId)) {
+            throw new NotAuthorizedException("Access denied");
+        }
+
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException("Account not found"));
+        if (!account.getClient().getId().equals(clientId)) {
+            throw new NotAuthorizedException("Access denied");
+        }
+
+        return transactionRepository.findAllByAccountId(accountId);
     }
 
 }
