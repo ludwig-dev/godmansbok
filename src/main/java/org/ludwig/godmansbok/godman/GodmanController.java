d3f9a33280c93ba949ed2d394d257f8474a4410c
Ludwig <00ludwighahn@gmail.com>
2025-05-31 17:51:48 +0200

    Create GodmanController with endpoint to retrieve the authenticated user's details

---
+++ src/main/java/org/ludwig/godmansbok/godman/GodmanController.java
@@ -0,0 +1,32 @@
+package org.ludwig.godmansbok.godman;
+
+import org.ludwig.godmansbok.godman.dto.GodmanDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/godman")
+public class GodmanController {
+    private final GodmanService godmanService;
+
+    public GodmanController(GodmanService godmanService) {
+        this.godmanService = godmanService;
+    }
+
+    @GetMapping("/me")
+    public GodmanDTO getCurrentUser(Authentication authentication) {
+        Long id = requireAuth(authentication);
+        return GodmanDTO.toDto(godmanService.getGodman(id));
+    }
+
+    private Long requireAuth(Authentication auth) {
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+        }
+        return Long.parseLong(auth.getName());
+    }
+}
