fc2b6116644b8a109544fc8a0c57dbe13dd42762
Ludwig <00ludwighahn@gmail.com>
2025-05-31 19:30:26 +0200

    ClientDTO: Add `id` field and update `toDto` method

--- src/main/java/org/ludwig/godmansbok/domain/clients/dto/ClientDTO.java
+++ src/main/java/org/ludwig/godmansbok/domain/clients/dto/ClientDTO.java
@@ -4,9 +4,9 @@ import org.ludwig.godmansbok.domain.clients.Client;
 
 import java.util.List;
 
-public record ClientDTO(String name, String personalNumber) {
+public record ClientDTO(Long id, String name, String personalNumber) {
     public static ClientDTO toDto(Client client) {
-        return new ClientDTO(client.getName(), client.getPersonalNumber());
+        return new ClientDTO(client.getId(), client.getName(), client.getPersonalNumber());
     }
 
     public static List<ClientDTO> toDtos(List<Client> clients) {
