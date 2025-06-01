9d6b06cac1d41c161fec9dd2b270a5c5ac42c604
Luwig Hahn <00ludwighahn@gmail.com>
2025-06-01 19:19:26 +0200

    LiabilityService: add method to create a liability

---
+++ src/main/java/org/ludwig/godmansbok/domain/liability/LiabilityService.java
@@ -0,0 +1,39 @@
+package org.ludwig.godmansbok.domain.liability;
+
+import org.ludwig.godmansbok.domain.clients.Client;
+import org.ludwig.godmansbok.domain.clients.ClientRepository;
+import org.ludwig.godmansbok.domain.liability.dto.LiabilityDTO;
+import org.ludwig.godmansbok.exceptions.NotAuthorizedException;
+import org.ludwig.godmansbok.exceptions.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LiabilityService {
+    private final LiabilityRepository liabilityRepository;
+    private final ClientRepository clientRepository;
+
+    public LiabilityService(LiabilityRepository liabilityRepository,
+                            ClientRepository clientRepository) {
+        this.liabilityRepository = liabilityRepository;
+        this.clientRepository = clientRepository;
+    }
+
+    public Liability createLiability(Long godmanId,
+                                     Long clientId,
+                                     LiabilityDTO dto) {
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not found"));
+        if (!client.getGodman().getId().equals(godmanId)) {
+            throw new NotAuthorizedException("Access denied");
+        }
+
+        Liability liability = new Liability();
+        liability.setCreditor(dto.creditor());
+        liability.setDebtStartOfYear(dto.debtStartOfYear());
+        liability.setDebtEndOfYear(dto.debtEndOfYear());
+        liability.setChangeAmount(dto.changeAmount());
+        liability.setAttachmentNumber(dto.attachmentNumber());
+        liability.setClient(client);
+        return liabilityRepository.save(liability);
+    }
+}
