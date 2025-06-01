fcee598c30667e406dcb14b84166adc792ce2511
Luwig Hahn <00ludwighahn@gmail.com>
2025-06-01 20:05:43 +0200

    LiabilityRepository: find all liabilities by client id

--- src/main/java/org/ludwig/godmansbok/domain/liability/LiabilityRepository.java
+++ src/main/java/org/ludwig/godmansbok/domain/liability/LiabilityRepository.java
@@ -2,5 +2,8 @@ package org.ludwig.godmansbok.domain.liability;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LiabilityRepository extends JpaRepository<Liability, Long> {
+    List<Liability> findAllByClientId(Long clientId);
 }
