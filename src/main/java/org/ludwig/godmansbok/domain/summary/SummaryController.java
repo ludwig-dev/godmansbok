bffd3eac3123d6aadccdb7557fc00935e055bb19
Ludwig <00ludwighahn@gmail.com>
2025-06-03 18:00:02 +0200

    AccountController: change endpoint to take in AuthenticationPrincipal

--- src/main/java/org/ludwig/godmansbok/domain/summary/SummaryController.java
+++ src/main/java/org/ludwig/godmansbok/domain/summary/SummaryController.java
@@ -1,5 +1,7 @@
 package org.ludwig.godmansbok.domain.summary;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +17,11 @@ public class SummaryController {
     @GetMapping
     public SummaryDTO getFullSummary(
             @PathVariable Long clientId,
-            @RequestParam(required = false) Integer year
+            @RequestParam(required = false) Integer year,
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
+        Long godmanId = Long.parseLong(userDetails.getUsername());
         int queryYear = (year != null) ? year : java.time.Year.now().getValue();
-        return summaryService.getFullSummaryForYear(clientId, queryYear);
+        return summaryService.getFullSummaryForYear(godmanId, clientId, queryYear);
     }
 }
