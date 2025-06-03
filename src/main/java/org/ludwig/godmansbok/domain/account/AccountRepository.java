6586424f66f28d65a88fb30038aa2107bf51b58a
Ludwig <00ludwighahn@gmail.com>
2025-06-03 17:55:50 +0200

    AccountRepository: create method findFirstByClientId

--- src/main/java/org/ludwig/godmansbok/domain/account/AccountRepository.java
+++ src/main/java/org/ludwig/godmansbok/domain/account/AccountRepository.java
@@ -3,7 +3,9 @@ package org.ludwig.godmansbok.domain.account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long>{
     List<Account> findAllByClientId(Long clientId);
+    Optional<Account> findFirstByClientId(Long clientId);
 }
